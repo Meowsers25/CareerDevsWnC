63230056ea958ae6b62584203ca81c3cc633edd9
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-10 17:00:23 +0000

    Initial commit

---
+++ version1.js
@@ -0,0 +1,23 @@
+var todos = ['item 1', 'item 2', 'item3'];
+
+//console.log('My todos: ', todos);
+
+todos.push('item 4');
+
+//console.log(todos);
+
+todos.push('item 5');
+
+//console.log(todos);
+
+todos[0] = 'item 1 updated';
+
+//console.log(todos);
+
+todos.splice(0, 1);
+
+//console.log(todos);
+
+todos.splice(3, 1);
+
+console.log(todos);
