75b51d53e352059cdc3860433aeb3c667e84ffa1
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-11 00:12:07 +0000

    Added version3.js

---
+++ version3.js
@@ -0,0 +1,30 @@
+//objects
+
+// var chris = {
+//     name: 'Chris',
+//     sayName: function() { //method is a property thats equal to a function
+//         console.log(this.name);
+//     }
+// };
+// chris.sayName();
+
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {
+        console.log('My todos', this.todos);
+    },
+    addTodo: function(todo) {
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, newValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    }
+};
+
+todoList.deleteTodo(1);
