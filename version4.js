6d8604e54bf93ce968ce0b8f026397915ac633d6
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-07-11 00:35:48 +0000

    Added version4.js

---
+++ version4.js
@@ -0,0 +1,34 @@
+//Booleans
+
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        console.log('My todos', this.todos);
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        //this.todos[position] = newValue;
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
+
+ todoList.addTodo('Boolean testing');
+// todoList.changeTodo(0, 'second try');
+
+todoList.toggleCompleted(0);
